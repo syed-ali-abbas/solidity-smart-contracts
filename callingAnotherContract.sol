00b9e139cb3b850cca3402f4f23ec09a350eb183
SYED ALI ABBAS <55765103+syed-ali-abbas@users.noreply.github.com>
2022-09-05 11:50:36 +0500

    calling another contract

---
+++ callingAnotherContract.sol
@@ -0,0 +1,50 @@
+//SPDX-License-Identifier:UNLICENSED
+pragma solidity 0.8.7;
+
+contract CallTestContract{
+
+function setX(TestContract _test, uint _x) external{
+    _test.setX(_x);
+}
+
+function getX(TestContract _test) external view returns (uint x){
+  x = _test.getX();
+  return x;
+}
+
+function setXandSendEther(address _test, uint _x) external payable {
+TestContract(_test).setXandRecieveEther{value:msg.value}(_x);
+}
+
+function getXandValue(address _test) external view returns (uint, uint){
+
+(uint x,uint value) = TestContract(_test).getXandValue();
+  return (x,value);
+}
+
+
+}
+
+contract TestContract{
+
+uint public x;
+uint public value=123;
+
+function setX(uint _x) external{
+    x=_x;
+}
+
+function getX() external view returns (uint){
+    return x;
+}
+
+function setXandRecieveEther(uint _x) external payable{
+    x=_x;
+    value=msg.value;
+}
+
+function getXandValue() external view returns (uint,uint){
+    return (x,value);
+}
+
+}
